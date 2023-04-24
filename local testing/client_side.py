31134cd65d9fb05e6d4fad0abff6962bc49c120d
navinraj <navin.raj@datakaveri.org>
2023-04-24 14:53:14 +0530

    added local testing

---
+++ local testing/client_side.py
@@ -0,0 +1,146 @@
+import pika
+import uuid
+import time
+import json
+
+def on_reply_message_received(ch, method, properties, body):
+    v = json.loads(body)
+    print(v)
+    channel.stop_consuming()
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+reply_queue = channel.queue_declare(queue='', exclusive=True)
+
+channel.basic_consume(queue=reply_queue.method.queue, auto_ack=True,
+    on_message_callback=on_reply_message_received)
+
+channel.queue_declare(queue='request-queue')
+
+cor_id = str(uuid.uuid4())
+print(f"Sending Request: {cor_id}")
+
+# json_object = {
+# "id": [
+# "iisc.ac.in/89a36273d77dac4cf38114fca1bbe64392547f86/rs.iudx.io/pune-env-flood/FWR053"
+# ],
+# "attr-query": "isValid==%7B%0A%22departmentid%22%3A%20%2222%22%2C%0A%22SRNID%22%3A%20%226958961347%22%2C%0A%22FName%22%3A%20%22Shailesh%22%2C%0A%22MName%22%3A%20%22Bhisham%22%2C%0A%22LName%22%3A%20%22J%22%2C%0A%22DOB%22%3A%20%221997-08-07%22%2C%0A%22Gender%22%3A%20%22M%22%2C%0A%22FatherHusbandName%22%3A%20%22Bhisham%22%2C%0A%22RationCardFamilyNo%22%3A%20%22226236000000%22%2C%0A%22RationCardMemberNo%22%3A%20%224%22%2C%0A%22MobileNumber%22%3A%20%227689586951%22%2C%0A%22EmailID%22%3A%20%22test%40gmail.com%22%2C%0A%22BankAccounNo%22%3A%20%22234567892%22%2C%0A%22IFSCCode%22%3A%20%22BKID0000806%22%2C%0A%22HouseBuildingApartmentNumber%22%3A%20%22S-11%22%2C%0A%22StreetRoadLane%22%3A%20%22Nehru%20nagar%22%2C%0A%22Landmark%22%3A%20%22Nehru%20nagar%22%2C%0A%22AreaLocalitySector%22%3A%20%22Bilaspur%22%2C%0A%22VillageTownCity%22%3A%20%22Bilaspur%22%2C%0A%22SubDistrictCode%22%3A%20%22752%22%2C%0A%22DistrictCode%22%3A%20%22545%22%2C%0A%22StateCode%22%3A%20%22300%22%2C%0A%22PinCode%22%3A%20%22491771%22%2C%0A%22LocationAddress%22%3A%20%22Bilaspur%22%2C%0A%22UrbanRural%22%3A%20%22Urban%22%2C%0A%22UrbanLocalBodyGramPanchayat%22%3A%20%22neharu%20nagar%22%2C%0A%22WardVillage%22%3A%20%226%22%0A%7D;departmentid==22",
+# "searchType": "latestSearch_attributeSearch",
+# "instanceID": "localhost:8443",
+# "applicableFilters": [
+# "ATTR",
+# "TEMPORAL"
+# ],
+# "publicKey": None
+# }
+
+# json_object= {
+# "id": [
+# "iisc.ac.in/89a36273d77dac4cf38114fca1bbe64392547f86/rs.iudx.io/pune-env-flood/FWR055"
+# ],
+# "temporal-query": {
+# "time": "2021-06-09T14:20:00Z",
+# "endtime": "2021-06-10T14:20:00Z",
+# "timerel": "during"
+# },
+# "attr-query": "year==2020-2021;schemecode==22",
+# "searchType": "temporalSearch_attributeSearch",
+# "instanceID": "localhost:8443",
+# "applicableFilters": [
+# "ATTR",
+# "TEMPORAL"
+# ],
+# "publicKey": None
+# }
+
+# json_object={
+#     "id": [
+#     "iisc.ac.in/89a36273d77dac4cf38114fca1bbe64392547f86/rs.iudx.io/pune-env-flood/FWR055"
+#     ],
+#     "attr-query": "villcode==2601001;surveyno==48ఉ1",
+#     "searchType": "temporalSearch_attributeSearch",
+#     "instanceID": "localhost:8443",
+#     "applicableFilters": [
+#     "ATTR",
+#     "TEMPORAL"
+#     ],
+#     "publicKey": None,
+#     "ppbNumber":"T26010010910"
+
+# }
+
+# json_object= {
+# "id": [
+# "iisc.ac.in/89a36273d77dac4cf38114fca1bbe64392547f86/rs.iudx.io/pune-env-flood/FWR055"
+# ],
+# "temporal-query": {
+# "time": "2022-08-22T12:01:05Z",
+# "endtime": "2022-08-22T12:01:05Z",
+# "timerel": "during"
+# },
+# "attr-query": "Finyear==2022;Season==2;DistCode==12;MandCode==248;VillCode==991211003;ppbnumber==T13010130530",
+# "searchType": "temporalSearch_attributeSearch",
+# "instanceID": "localhost:8443",
+# "applicableFilters": [
+# "ATTR",
+# "TEMPORAL"
+# ],
+# "publicKey": None,
+# "ppbNumber":"T13010130530"
+# }
+
+
+
+json_object = {
+	"id": [
+		"rythubandhu.telangana.gov.in/1668984090c7e92e4c39e1e5d29300e6a9b2a11e/gateway.adex.org.in/rythubandhu-schemes/get-cb-data"
+	],
+	# "temporal-query": {
+	# 	"time": "2022-08-22T12:01:05Z",
+	# 	"endtime": "2022-08-22T110:05Z",
+	# 	"timerel": "during"
+	# },
+	# "attr-query": "Finyear==2022;Season==1;DistCode==26;MandCode==480;VillCode==2601001",
+	# "searchType": "temporalSearch_attributeSearch",
+	# "instanceID": "localhost:8443",
+	# "applicableFilters": [
+	# 	"ATTR",
+	# 	"TEMPORAL"
+	# ],
+	# "publicKey": "None",
+	"ppbNumber": "T13010130530"
+}
+
+
+
+
+# json_object={
+#    "id":[
+#       "datakaveri.org/5841d4c98a9ef8ab708e01a3c5fc1ab955e6ada0/rs.adex.org.in/khammam-crop-test/mandal-crop-info-test1"
+#    ],
+#    "attr-query":"ppbnumber==T13010130530",
+#    "searchType":"latestSearch_attributeSearch",
+#    "instanceID":"localhost:8081",
+#    "applicableFilters":[
+      
+#    ],
+#    "publicKey":"",
+#    "ppbNumber":"T13010130530"
+# }
+
+message = json.dumps(json_object)
+
+
+channel.basic_publish('', routing_key='request-queue', properties=pika.BasicProperties(
+    reply_to=reply_queue.method.queue,
+    correlation_id=cor_id
+), body=message)
+
+print("Starting Client")
+
+channel.start_consuming()
+
---
+++ local testing/get_cb_data_local.py
@@ -0,0 +1,319 @@
+import datetime
+import json
+import logging
+import sys
+import urllib.error
+from configparser import ConfigParser
+from urllib.parse import urlparse
+from urllib.request import Request, urlopen
+
+import pika
+import xmltodict
+
+config = ConfigParser(interpolation=None)
+config.read("../config/config_file.ini")
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+time_format = "%Y-%m-%dT%H:%M:%SZ"
+time_formatter = "%Y-%m-%dT%H:%M:%S.%f"
+
+class RabbitMqServerConfigure():
+
+    def __init__(self,host,queue):
+
+        """ Server initialization   """
+
+
+        self.host = host
+        self.queue = queue
+
+class rabbitmqServer(object):
+
+    def __init__(self, server):
+
+        """
+        Establishing the connection
+        :param server: Object of class RabbitMqServerConfigure
+        :return:None
+        """
+
+        self.server = server 
+
+
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=self.server.host
+                )
+            )
+
+        self.channel = self.connection.channel()
+
+        print("Server started waiting for Messages ")
+        
+    def startserver(self, on_request):
+
+        """
+        Starting the server to consume
+        :param on_reuest:callback
+        :return:None
+        """
+        # self.channel.basic_qos(prefetch_count=1)
+
+        self.channel.basic_consume(
+            queue=self.server.queue,
+            on_message_callback=on_request,
+            auto_ack=True
+            )
+
+        self.channel.start_consuming()  
+        
+        
+    def publish(self, payload, rout_key, corr_id, method):
+
+        """
+        :param payload: JSON payload
+        :param rout_key: reply queue
+        :param corr_id: correlation id 
+#         :return: None
+#         """
+
+        message = json.dumps({"message": payload, "routing_key":rout_key, "correlation_id":corr_id})
+        self.channel.basic_publish(
+            exchange='', 
+            routing_key=rout_key, 
+            properties=pika.BasicProperties(correlation_id = corr_id),
+            body=message
+            )
+        # self.channel.basic_ack(delivery_tag=method.delivery_tag)
+
+        print("Published Message: {}".format(payload))
+
+class get_cb_data:
+
+    def __init__(self, url, queue, iudx_username, iudx_password):
+
+        """ Variable initialization   """
+
+        self.url = url
+        self.queue = queue
+        self.iudx_username = iudx_username
+        self.iudx_password = iudx_password
+
+    def process_request(self, ch, method, properties, body):
+
+        """
+        Processing the request
+        :param method: 
+        :param properties: properties consists of required ids
+        :param body: request json response to form the api
+        :return:None
+        """
+
+        logging.info(".......RequestJson received......")
+        self.json_object = json.loads(body)
+        self.rout_key=properties.reply_to
+        self.corr_id = properties.correlation_id
+        self.method = method
+        
+        logging.info(".........Forming the api.........")
+
+        self.form_api()
+
+    def form_api(self):
+        
+        """
+        Forming the API
+        :return:None
+        """
+        error_dict = {}
+        
+        try:
+            query=self.json_object["searchType"]
+            query_list=query.split("_")
+
+            if "attributeSearch" in query_list:
+                attribute_dict = self.attribute_end_dict("attr-query")
+
+            if "temporalSearch" in query_list:
+                temporal_dict = self.temporal_end_dict("temporal-query")
+            
+            if attribute_dict and temporal_dict:
+                end_dict = {**attribute_dict, **temporal_dict}
+
+            else:
+                end_dict = attribute_dict
+
+            self.getData(end_dict)
+            return
+
+        except KeyError as ek:
+
+            logging.error("A Key error occurred: {}".format(ek))
+
+            error_dict["status"] = 400
+            error_dict["details"] = f"Keyerror: Key {str(ek)} not found in the json request body"
+
+        except Exception as e:  
+
+            logging.error("An Unknown Error occurred: {}".format(e))
+
+            error_dict["status"] = 400
+            error_dict["details"] = str(e)
+
+        server.publish(error_dict, self.rout_key, self.corr_id, self.method) 
+
+    def attribute_end_dict(self, query_type):
+
+        """
+        Forming the end point
+        :params query_type: attribute query
+        :return attribute endpoint: attribute endpoint consists of attribute queries
+        """
+
+        attr_list = self.json_object[query_type].split(";")
+        
+        attr_dict = {}
+
+        for attr in attr_list: 
+
+            if "Finyear" in attr:
+                attr_dict["Finyear"] = attr.replace("Finyear==","")
+            if "Season" in attr:
+                attr_dict["Season"] = attr.replace("Season==","")
+            if "DistCode" in attr:
+                attr_dict["DistCode"] = attr.replace("DistCode==","")
+            if "MandCode" in attr:
+                attr_dict["MandCode"] = attr.replace("MandCode==","")
+            elif "VillCode" in attr:
+                attr_dict["VillCode"] = attr.replace("VillCode==","")
+
+        return attr_dict
+    
+
+    def temporal_end_dict(self, query_type):
+
+        """
+        Forming the temporal end point
+        :params query_type: temporal query 
+        :return temporal end point: temporal endpoint consists of temporal queries
+        """
+        temporal_dict = {}
+        if self.json_object[query_type]["timerel"] == "during":
+
+            start_date_value = datetime.datetime.strptime(self.json_object[query_type]["time"], time_format)
+            temporal_dict["StartDate"] = start_date_value.strftime(time_formatter)
+            
+            end_date_value = datetime.datetime.strptime(self.json_object[query_type]["endtime"], time_format)
+            temporal_dict["EndDate"] = end_date_value.strftime(time_formatter)
+
+            print(temporal_dict)
+
+            return temporal_dict
+
+
+    def getData(self, end_dict):
+
+        """
+        Fetching from seeding api
+        :params end_point: end point which consists of attribute or temporal query
+        :return None:
+        """
+
+        dictionary ={}
+
+        try:
+            
+            url = self.url
+            
+            payload =  """<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                            <soap:Body>
+                                <Get_CB_Data xmlns="http://tempuri.org/">
+                                <WS_UserName>{}</WS_UserName>
+                                <WS_Password>{}</WS_Password>
+                                <Finyear>{}</Finyear>
+                                <Season>{}</Season>
+                                <DistCode>{}</DistCode>
+                                <MandCode>{}</MandCode>
+                                <VillCode>{}</VillCode>
+                                <StartDate>{}</StartDate>
+                                <EndDate>{}</EndDate>
+                                </Get_CB_Data>
+                            </soap:Body>
+                        </soap:Envelope>""".format(self.iudx_username, self.iudx_password, end_dict["Finyear"], end_dict["Season"], end_dict["DistCode"], end_dict["MandCode"], end_dict["VillCode"], end_dict["StartDate"], end_dict["EndDate"])
+
+            headers = {
+                'Content-Type': 'text/xml'
+                }
+
+            req = Request(url, payload.encode('utf-8'), headers=headers )
+            response = urlopen(req)
+            status = response.getcode()
+
+            dictionary = self.fetch_response(status, response, dictionary)
+
+        except urllib.error.HTTPError as eh:
+            
+            logging.error("An Http Error occurred: {}".format(eh))
+            
+            dictionary["status"] = eh.code 
+            dictionary["details"] =  eh.reason
+            
+        except urllib.error.URLError as eu:
+            
+            logging.error("An URL Error occurred: {}".format(eu))
+            
+            dictionary["status"] = eu.reason.args[0]
+            dictionary["details"] =  str(eu.reason)
+
+        except Exception as e:
+            
+            logging.error("An Unknown Error occurred: {}".format(e))
+            
+            dictionary["status"] = 400
+            dictionary["details"] = 'An unknown error occurred while processing the request on the server'
+
+
+        server.publish(dictionary, self.rout_key, self.corr_id, self.method)
+
+    def fetch_response(self, status, response, dictionary):
+        
+        """
+        Fetching the response and status code from URL
+        :params status: Status of the response
+        :params response: response from the api
+        :return dictionary: dictionary which consists of status and results/details
+        """
+        
+        resp_dict = xmltodict.parse(response.read())
+
+        if status==200:
+            soap = resp_dict["soap:Envelope"]["soap:Body"]
+
+            if not soap.get("soap:Fault", None):
+                response_json = json.loads(soap["Get_CB_DataResponse"]["Get_CB_DataResult"])
+                success_flag = response_json["SuccessFlag"]
+                success_msg = response_json["SuccessMsg"]
+
+            if success_flag == "E":
+                dictionary['statusCode'] =  204
+                dictionary["details"] = success_msg
+            
+            else:
+                dictionary['statusCode'] =  status
+                dictionary["results"] = response_json["Data"]
+
+
+        return dictionary
+
+if __name__ == '__main__':
+
+
+    host = config["local"]["host"]
+    queue = config["local"]["queue"]
+    url = config["cb_data_url"]["url"]
+    iudx_username = config["iudx_credentials"]["username"]
+    iudx_password = config["iudx_credentials"]["password"]
+    cd = get_cb_data(url, queue, iudx_username, iudx_password)
+    serverconfigure = RabbitMqServerConfigure( host, queue)
+    server = rabbitmqServer(server=serverconfigure)
+    server.startserver(cd.process_request)
